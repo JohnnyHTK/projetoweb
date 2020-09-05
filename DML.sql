04dc09a03f21845233528d223b5c13b55f014b60
herculespsilva <herculespsilva@hotmail.com>
2020-09-05 20:51:23 +0000

    DML corrigido

--- DML.sql
+++ DML.sql
@@ -1,8 +1,44 @@
-    -- SINGLE TABLE
-    --pes_nome, pes_cpf, tipo, atr_fama, dub_especialidade
-
     --Diretor
-    INSERT INTO myTable (pes_nome,pes_cpf,tipo) VALUES ("Michael Cooke","53565928899","T"),("Oliver Huffman","41829772099","T"),("Jonah Hamilton","59786031699","T"),("Tate Blanchard","69353646799","T"),("Magee Harrell","13924109099","T"),("Wayne Gilbert","38257998299","T"),("Rigel Preston","23302079099","T"),("Asher George","53225209899","T"),("Victor Reid","83625616199","T"),("Evan Curry","06402446999","T");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo) VALUES ("Luke Winters","02429130099","T");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo) VALUES ("Hop Bowman","29949872899","T");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo) VALUES ("Preston Bailey","18252066499","T");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo) VALUES ("Connor Hays","53538461799","T");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo) VALUES ("Damian George","27870155399","T");
 
-    --fmg_filmagem
+    --ator
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Owen Simpson","16795546299","A","Musica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Cairo Head","45516724899","A","Musica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Tyler Briggs","38314493399","A","Musica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Clark Knox","47725317999","A","Musica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Aladdin Irwin","48361232399","A","Musica");
     
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Abdul Mccray","39774355799","A","Teatro");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Kareem Hyde","74229628099","A","Teatro");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Beck Berg","43302315599","A","Teatro");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Erich Waters","68470720599","A","Teatro");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Christopher Stark","42446110799","A","Teatro");
+
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Quinlan Levine","20774908199","A","Dança");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Knox Ayala","92563485999","A","Dança");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Quentin French","37725918799","A","Dança");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Ashton Mcgee","34583196199","A","Dança");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,atr_fama) VALUES ("Wallace Nelson","33544063799","A","Dança");
+
+    --duble
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Kennan Black","80854941099","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Joel Camacho","20545039799","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Louis Mccray","51455458499","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Kenneth Wolfe","75319907999","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Hakeem Sweet","71786288799","D","Defesa Pessoal");
+
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Cameron Stanley","25494235199","D","Direção Automobilistica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Stewart Smith","90575932699","D","Direção Automobilistica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Malik Ball","11657387299","D","Direção Automobilistica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Joshua Wood","20491969599","D","Direção Automobilistica");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Damon Gallagher","02752005499","D","Direção Automobilistica");
+
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Neville Keller","57244884899","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Clarke Stark","27820267199","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Kennedy Franco","33832283899","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Flynn May","93250208199","D","Defesa Pessoal");
+    INSERT INTO pes_pessoa (pes_nome,pes_cpf,tipo,dub_especialidade) VALUES ("Omar Gutierrez","00645656299","D","Defesa Pessoal");
